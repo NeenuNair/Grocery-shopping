6a459db8d60cae6a7c9441cb150df3755311dbec
unknown <you@example.com>
2020-05-03 10:10:50 +0530

    updated on 03.05.2020

---
+++ ajax.js
@@ -0,0 +1,30 @@
+function ajax(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+    
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+
+            var Glist = response.list;
+
+            var output = "<table>";
+            for (var i = 0; i < Glist.length; i++) {
+
+                output += "<tr> <td>" + Glist[i].serialnumber + "</td><td>" +
+                    Glist[i].items + "</td><td>" +
+                    Glist[i].quantity + "</td><td>" +
+                    Glist[i].unit + "</td><td>" +
+                    Glist[i].department + "</td><td>" +
+                    Glist[i].notes + "</td></tr>";
+            }
+            output += "</table>";
+            document.getElementById("demo").innerHTML = output;
+
+
+        }
+    };
+    xhttp.open("GET", "item.json", true);
+    xhttp.send();
+}
+
+
